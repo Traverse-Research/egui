d590d606d50ba4e874fa50850d8d581775b5cf5f
Jasper Bekkers <jasper@traverseresearch.nl>
2024-11-22 10:12:53 +0100

    Make webbrowser non-optional, and version 1

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -977,14 +977,8 @@ pub fn update_viewport_info(
 }
 
 fn open_url_in_browser(_url: &str) {
-    #[cfg(feature = "webbrowser")]
     if let Err(err) = webbrowser::open(_url) {
         log::warn!("Failed to open url: {}", err);
-    }
-
-    #[cfg(not(feature = "webbrowser"))]
-    {
-        log::warn!("Cannot open url - feature \"links\" not enabled.");
     }
 }
 
